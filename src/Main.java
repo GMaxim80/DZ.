be70280b1c084647dc1658548b5a86c435d1bb49
GMaxim80 <mgubanov80@gmail.com>
2022-07-13 22:56:47 +0300

    Inicial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2000;
+        int miles = price / 20;
+
+        System.out.println("Сколько миль будет начислено");
+        System.out.println(miles + "миль");
+    }
+}
